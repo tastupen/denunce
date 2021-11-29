b79b7e51ab72713efbf1016811c81c77a4588e14
tastupen <tatsuprogramming0828@gmail.com>
2021-11-29 20:53:05 +0000

    02 create categories table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+major_category_names = ["愚痴のカテゴリー"]
+
+complaints_categories = ["会社の愚痴", "家族の愚痴", "友達の愚痴", "嫁の愚痴", "旦那の愚痴", "人に言えない愚痴"]
+
+major_category_names.each do |major_category_name|
+  if major_category_name == "愚痴のカテゴリー"
+        
+    complaints_categories.each do |complaints_category|
+      Category.create(
+        name: complaints_category,
+        description: complaints_category,
+        major_category_name: major_category_name
+      )
+    end
+  end
+end
